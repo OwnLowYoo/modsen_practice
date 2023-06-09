bab50e1cb1d0462fd4c577950c98e4c5cfa882ca
Gleb Katsko <BelgOkstak@gmail.com>
2023-06-09 13:30:35 +0300

    missed

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from "./components/NavBar";
+import Login from "./components/user/Login/Login";
+
+const App = () => {
+    return (
+        <>
+        <Login />
+        <NavBar />
+        </>
+    )
+};
+
+export default App;
---
+++ client/src/components/user/Login/Login.js
@@ -0,0 +1,116 @@
+import React, {useEffect, useRef, useState} from 'react';
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    IconButton,
+    TextField
+} from "@mui/material";
+import {useValue} from "../../../context/ContextProvider";
+import {Close, Send} from "@mui/icons-material";
+import PasswordField from "../PasswordField";
+
+const Login = () => {
+    const { state:{openLogin}, dispatch} = useValue()
+    const [title, setTitle] = useState('Вход')
+    const [isRegister, setIsRegister] = useState(false)
+    const nameRef = useRef()
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const confirmPasswordRef = useRef()
+
+    const handleClose = () => {
+        dispatch({type:'CLOSE_LOGIN'})
+    }
+    
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    useEffect (() =>{
+        isRegister ? setTitle('Регистрация') : setTitle('Вход');
+    }, [isRegister])
+
+    return (
+        <Dialog
+        open={openLogin}
+        onClose={handleClose}
+        >
+            <DialogTitle>
+                {title}
+                <IconButton
+                sx={{
+                    position:'absolute',
+                    top:8,
+                    right:8,
+                    color:(theme) => theme.palette.grey[500]
+                }}
+                onClick={handleClose}
+                >
+                    <Close />
+                </IconButton>
+            </DialogTitle>
+            <form onSubmit={handleSubmit}>
+                <DialogContent dividers>
+                    <DialogContentText>
+                        Пожалуйста заполните информацию ниже:
+                    </DialogContentText>
+                    {isRegister &&
+                    <TextField
+                        autoFocus
+                        margin='normal'
+                        variant='standard'
+                        id='name'
+                        label='Имя'
+                        type='text'
+                        fullWidth
+                        inputRef={nameRef}
+                        inputProps={{minLength:2}}
+                        required
+                        />
+                        }
+                    <TextField
+                        autoFocus={!isRegister}
+                        margin='normal'
+                        variant='standard'
+                        id='email'
+                        label='Почта'
+                        type='email'
+                        fullWidth
+                        inputRef={emailRef}
+                        required
+                    />
+                    <PasswordField {...{passwordRef}} />
+                    {isRegister &&
+                    <PasswordField
+                        passwordRef={confirmPasswordRef}
+                        id='confirmPassword'
+                        label='Подтвердите Пароль' />
+                    }
+                </DialogContent>
+                <DialogActions>
+                    <Button
+                        type='submit'
+                        variant='contained'
+                        endIcon={<Send />}>
+                        Отправить
+                    </Button>
+                </DialogActions>
+            </form>
+            <DialogActions sx={{ justifyContent:'left', p:'5px 24px'}}>
+                {isRegister
+                ? 'Уже есть аккаунт?'
+                : 'Нет аккаунта?'}
+                <Button onClick={() => setIsRegister(!isRegister)}>
+                    {isRegister ? 'Войти' : 'Зарегистрироваться'}
+                </Button>
+
+            </DialogActions>
+        </Dialog>
+    );
+};
+
+export default Login;
